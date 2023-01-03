873c8c9d1b32fffc71a03c43bcfcacdd043e195d
Bolin Huang <ibh@live.com>
2023-01-02 23:02:39 -0800

    refactor with sveltekit

---
+++ front-end/src/routes/Header.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { page } from '$app/stores';
+    import logo from '$lib/images/favicon-32x32.png';
+    import github from '$lib/images/github.svg';
+</script>
+
+<header>
+    <div id="logo">
+        <!-- <img src={logo} alt="logo" /> -->
+        OPAQUE
+    </div>
+
+    <!-- <nav>
+        <ul>
+            <li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+                <a href="/">Home</a>
+            </li>
+            <li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+                <a href="/about">About</a>
+            </li>
+        </ul>
+    </nav> -->
+</header>
+
+<style>
+    header {
+        display: flex;
+        justify-content: space-between;
+    }
+</style>
